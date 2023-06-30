4d63e112ed0f358dcb98e8562eda25c9b8452b5e
samuelerm08 <samuelerm08@gmail.com>
2023-06-30 00:15:32 -0300

    final changes

--- app/src/main/java/com/example/chucknorrisjokesapi_samuel_rivera/Joke.kt
+++ app/src/main/java/com/example/chucknorrisjokesapi_samuel_rivera/Joke.kt
@@ -3,5 +3,5 @@ package com.example.chucknorrisjokesapi_samuel_rivera
 import com.google.gson.annotations.SerializedName
 
 data class Joke (
-    @SerializedName("value") val joke: String,
+    @SerializedName("value") val joke: String
 )
--- app/src/main/java/com/example/chucknorrisjokesapi_samuel_rivera/JokeByCategory.kt
+++ app/src/main/java/com/example/chucknorrisjokesapi_samuel_rivera/JokeByCategory.kt
@@ -1,4 +1,48 @@
 package com.example.chucknorrisjokesapi_samuel_rivera
 
-class JokeByCategory {
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class JokeByCategory : AppCompatActivity() {
+    private lateinit var jokeOnDetail: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.category_detail)
+        jokeOnDetail = findViewById(R.id.detailJokeTitle)
+        val bundle = intent.extras
+        val category = bundle?.getString("category", "")
+        getJokeByCategory(category.toString())
+    }
+
+    private fun getJokeByCategory(category: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val request = fetchData().create(IChuckService::class.java)
+                .getJokeByCategory("random?category=${category}")
+            val response = request.body()
+            try {
+                runOnUiThread {
+                    if (request.code() == 200) {
+                        val jokeValue = response?.joke
+                        if (jokeValue != null) jokeOnDetail.text = jokeValue.toString()
+                    }
+                }
+            } catch (e: Exception) {
+                throw Exception(e)
+            }
+        }
+    }
+
+    private fun fetchData(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.chucknorris.io/jokes/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
